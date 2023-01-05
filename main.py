4a52509e4cbcc7ed23a971ce268b78de5a500e30
IsaacCrane <108107013+IsaacCrane@users.noreply.github.com>
2023-01-05 12:58:31 -0800

    hey asa you suck

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class piece:
 
 class move:
     def __init__(self,target,position,victim):
-        self.target = target
+        self.target = target #the piece you are moving
         self.position = position
         self.victim = victim
 
@@ -18,8 +18,28 @@ board = [[0, 1, 0, 1, 0, 1, 0, 1], [1, 0, 1, 0, 1, 0, 1, 0],
          [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0],
          [0, 2, 0, 2, 0, 2, 0, 2], [2, 0, 2, 0, 2, 0, 2, 0]]
+realBoard = []
+for q in range(len(board)):
+    realBoard.append([])
+    for j in range(len(board[q])):
+        if board[q][j] == 1:
+            realBoard.append(tile(1, 'pawn'))
+        if board[q][j] == 2:
+            realBoard.append(tile(2, 'pawn'))
+        if board[q][j] == 0:
+            realBoard.append(None)
+def findAllMoves(board)
+def findMove(board, moving): #the board is the board. moving is an array where moving[0]][moving][1] is the index for the 2d board array to point to a speciic square.
+    movingPiece = board[moving[0]][moving][1] #hopefully either 1 or 2. if someone asked for nothing to move then it would be 0.
+    if movingPiece.player == 1:
+        checkingFor = 2
+        direction = 1
+    if movingPiece.player == 2:
+        checkingFor = 1
+        direction = -1
+    if movingPiece.type == 'king':
+        for i in range(4):
 
-def functionMove(board, moving): #the board is the board. moving is an array where moving[0]][moving][1] is the index for the 2d board array to point to a speciic square.
-    movingColor = board[moving[0]][moving][1] #hopefully either 1 or 2. if someone asked for nothing to move then it would be 0.
-    for 
+    if movingPiece.type == 'pawn':
+        if board[moving[0]+direction][moving][1]]:
     
