1fc986d60bc2d0d9024d77ef692973cf7f226167
IsaacCrane <108107013+IsaacCrane@users.noreply.github.com>
2023-01-11 12:43:59 -0800

    hello can you hear me

--- train.py
+++ train.py
@@ -15,10 +15,8 @@ def sigmoid(x):
 
 
 class Layer:
-    def __init__(self):
-        self.input = None
-        self.output = None
-
+    def __init__(self, input, output):
+        self.weights = np.array(input, output)
     # computes the output Y of a layer for a given input X
     def forward_propagation(self, input):
         raise NotImplementedError
@@ -26,3 +24,4 @@ class Layer:
     # computes dE/dX for a given dE/dY (and update parameters if any)
     def backward_propagation(self, output_error, learning_rate):
         raise NotImplementedError
+
